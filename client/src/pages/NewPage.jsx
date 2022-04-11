1d42ea08370f4d1fb6b26947d487f8df05deea64
1107-itssamyak <samyak.mehta11@gmail.com>
2022-04-11 21:40:26 +0530

    adding new page(for creating new dashboard)

---
+++ client/src/pages/NewPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function NewPage() {
+    return (
+        <>
+            <div className="home">
+                <a href="/">home</a>
+            </div>
+            NewPage
+        </>
+    )
+}
+
+export default NewPage;
